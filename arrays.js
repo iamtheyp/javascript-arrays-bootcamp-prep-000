6a16d11416310bf0b756ead52c2a414da26626a5
The YP <nathon.green@gmail.com>
2018-06-09 19:36:52 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,11 @@
+/*var chocolateBars = [
+  "snickers", 
+  "hundred grand", 
+  "kitkat", 
+  "skittles"
+  ];*/
+function addElementToBeginningOfArray(tempArray,element){
+  var myArray = tempArray;
+  myArray.push(element);
+  return myArray;// change this later
+}//addElementToBeginningOfArray
